f67b518f9f9385cd88ebe59a3ab90d248ef75af2
Aman Menda <aman.menda@epitech.eu>
2022-11-20 14:46:16 +0100

    init repository

---
+++ grrs/target/debug/deps/grrs-86cc4481a281c633.d
@@ -0,0 +1,5 @@
+/home/aman/rust-cli/grrs/target/debug/deps/grrs-86cc4481a281c633: src/main.rs
+
+/home/aman/rust-cli/grrs/target/debug/deps/grrs-86cc4481a281c633.d: src/main.rs
+
+src/main.rs:
---
+++ grrs/target/debug/grrs.d
@@ -0,0 +1,1 @@
+/home/aman/rust-cli/grrs/target/debug/grrs: /home/aman/rust-cli/grrs/src/main.rs
